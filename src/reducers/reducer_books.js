2d1a01db5992b211e4b1ace65677d4719652c0da
theperfectfuel <robert@theperfectfuel.com>
2017-11-03 01:34:47 -0700

    Initial commit with basic working book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        { title: 'Ask the Dust' },
+        { title: 'Harry Potter' },
+        { title: 'Eloquent Javascript' },
+        { title: 'Moby Dick' },
+        { title: 'Catcher in the Rye' }
+    ]
+}
